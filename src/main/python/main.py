3085461364da0925f6e9a83a5b626a71f5b24563
AINDUSTRIES <alexandredaoust4@gmail.com>
2021-01-11 17:21:45 +0100

    Dev pt3

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -14,14 +14,17 @@ class MainWindow(QtWidgets.QWidget):
         self.create_layouts()
         self.add_widgets_to_layout()
         self.setup_connetions()
+        self.rpc_start()
 
     def create_widgets(self):
         self.btn_set = QtWidgets.QPushButton("Set as Status")
         self.btn_clear = QtWidgets.QPushButton("Clear current Status")
-        self.cb_choice = QtWidgets.QComboBox()
+        self.cb_icon_choice = QtWidgets.QComboBox()
+        self.le_details = QtWidgets.QLineEdit("Top Line")
+        self.le_status = QtWidgets.QLineEdit("Bottom Line")
 
     def modify_widgets(self):
-        self.cb_choice.addItems(["VisualStudioCode", "Youtube", "Technical Stuff"])
+        self.cb_icon_choice.addItems(["VisualStudioCode", "Youtube", "Technical Stuff"])
 
     def create_layouts(self):
         self.main_layout = QtWidgets.QGridLayout(self)
@@ -32,12 +35,24 @@ class MainWindow(QtWidgets.QWidget):
         self.main_layout.addWidget(self.btn_clear, 2,1,1,2)
 
     def setup_connetions(self):
-        pass
+        self.btn_set.clicked.connect(self.rpc_set)
+        self.btn_clear.clicked.connect(self.rpc_clear)
+
+    def rpc_clear(self):
+        self.presence.clear()
 
     def rpc_start(self):
         self.presence = rpc.Presence("797892938201432125")
         self.presence.connect()
-        self.presence.update(details="Setting up custom status...")
+        self.presence.update(details="Setting up custom status...", state="Coded by AINDUSTRIES")
+
+    def rpc_set(self):
+        if self.cb_icon_choice.currentText() == "VisualStudioCode":
+            self.presence.update(details="Coding Stuff in", state="Visual Studio Code")
+
+    def closeEvent(self, event=QtGui.QCloseEvent):
+        self.presence.clear()
+        self.presence.close()
 
 if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
